7559aa3723b2ba2b927df37a686c6db2caf2fcb1
tanguyn <nguyen03012004@gmail.com>
2025-04-19 09:13:57 +0700

    backend

--- QLKS1.API/Repositories/Implementations/ChiTietHoaDonRepository.cs
+++ QLKS1.API/Repositories/Implementations/ChiTietHoaDonRepository.cs
@@ -1,33 +1,33 @@
-using System.Data;
-using Dapper;
-using QLKS1.API.Repositories.Interfaces;
+// using System.Data;
+// using Dapper;
+// using QLKS1.API.Repositories.Interfaces;
 
 
-public class ChiTietHoaDonRepository : IDatPhongRepository
-{
-    private readonly IDbConnection _db;
+// public class ChiTietHoaDonRepository : IDatPhongRepository
+// {
+//     private readonly IDbConnection _db;
 
-    public ChiTietHoaDonRepository(IDbConnection db)
-    {
-        _db = db;
-    }
+//     public ChiTietHoaDonRepository(IDbConnection db)
+//     {
+//         _db = db;
+//     }
 
-    public async Task<IEnumerable<ChiTietHoaDon>> GetAllChiTietHoaDon()
-    {
-        var chitiethoadonList = (await _db.QueryAsync<ChiTietHoaDon>(
-        "sp_GetAllChiTietHoaDon", commandType: CommandType.StoredProcedure)).ToList();
+//     public async Task<IEnumerable<ChiTietHoaDon>> GetAllChiTietHoaDon()
+//     {
+//         var chitiethoadonList = (await _db.QueryAsync<ChiTietHoaDon>(
+//         "sp_GetAllChiTietHoaDon", commandType: CommandType.StoredProcedure)).ToList();
 
-        return chitiethoadonList;
-    }
+//         return chitiethoadonList;
+//     }
 
-    public async Task<ChiTietHoaDon?> GetChiTietHoaDonByIdAsync(int MaHoaDon)
-    {
-        var parameters = new DynamicParameters();
-        parameters.Add("@MaHoaDon", MaHoaDon);
+//     public async Task<ChiTietHoaDon?> GetChiTietHoaDonByIdAsync(int MaHoaDon)
+//     {
+//         var parameters = new DynamicParameters();
+//         parameters.Add("@MaHoaDon", MaHoaDon);
 
-        var chiTietHoaDon = await _db.QueryFirstOrDefaultAsync<ChiTietHoaDon>(
-            "sp_GetChiTietHoaDonById", parameters, commandType: CommandType.StoredProcedure);
+//         var chiTietHoaDon = await _db.QueryFirstOrDefaultAsync<ChiTietHoaDon>(
+//             "sp_GetChiTietHoaDonById", parameters, commandType: CommandType.StoredProcedure);
 
-        return chiTietHoaDon;
-    }
-}
+//         return chiTietHoaDon;
+//     }
+// }
--- QLKS1.API/Repositories/Interfaces/IChiTietHoaDonRepository.cs
+++ QLKS1.API/Repositories/Interfaces/IChiTietHoaDonRepository.cs
@@ -1,12 +1,12 @@
 
-namespace QLKS1.API.Repositories.Interfaces
-{
-    public interface IChiTietHoaDonRepository
-    {
-        Task<IEnumerable<ChiTietHoaDon>> GetAllChiTietHoaDon();
-        Task<ChiTietHoaDon?> GetChiTietHoaDonByIdAsync(int MaHoaDon);
-        // Task<HoaDon> CreateAsync(HoaDon hoaDon);
-        // Task<HoaDon?> UpdateAsync(int IDHoaDon, HoaDon hoaDon);
-        // Task<bool> DeleteAsync(int IDHoaDon);
-    }
-}
+// namespace QLKS1.API.Repositories.Interfaces
+// {
+//     public interface IChiTietHoaDonRepository
+//     {
+//         Task<IEnumerable<ChiTietHoaDon>> GetAllChiTietHoaDon();
+//         Task<ChiTietHoaDon?> GetChiTietHoaDonByIdAsync(int MaHoaDon);
+//         // Task<HoaDon> CreateAsync(HoaDon hoaDon);
+//         // Task<HoaDon?> UpdateAsync(int IDHoaDon, HoaDon hoaDon);
+//         // Task<bool> DeleteAsync(int IDHoaDon);
+//     }
+// }
