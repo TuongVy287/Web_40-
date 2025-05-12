81e6af9e354f15f0fc2bd9b58d2ae2de58c815dd
Vy <nguyenthituongvy28072004@gmail.com>
2025-05-12 12:31:16 +0700

    Room

--- QLKS1.API/obj/Debug/net8.0/QLKS1.API.AssemblyInfo.cs
+++ QLKS1.API/obj/Debug/net8.0/QLKS1.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24856129debbe80f660a8c47917c6730fb163d1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b8e51de4c09a1132ed65a878139e3e555c02f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
