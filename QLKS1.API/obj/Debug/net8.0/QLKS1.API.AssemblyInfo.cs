5438fe91359609adb969062f87a50eff5fec71d6
bba2112 <hiambba2112@gmail.com>
2025-05-12 12:39:22 +0700

    12h39 12/5

--- QLKS1.API/obj/Debug/net8.0/QLKS1.API.AssemblyInfo.cs
+++ QLKS1.API/obj/Debug/net8.0/QLKS1.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f90b758c24073c8cd5d7b942f31615d7815ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c303ae77348e795bdbaff67e89359bb018579647")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLKS1.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
